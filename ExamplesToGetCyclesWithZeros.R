20de25b4adb6141bc9e52c7d87dbe951ed5fb91c
SP <spetrov1@iit.edu>
2016-05-16 12:06:26 -0500

    Add files via upload
    Do the p1-based-walk on a nxn table with structural zeros allowed; uses undirected moves from p1. 
    This code has to be fixed because it has silly input structure.

---
+++ ExamplesToGetCyclesWithZeros.R
@@ -0,0 +1,111 @@
+##########################################################################################
+# This file loads the necessary functions from GetCyclesWithZeros.R, 
+# as well as background code p1walk.R, 
+# and has an example on how to run the code in general. 
+#
+# Author:   Sonja Petrovic <Sonja.Petrovic@iit.edu>
+# For: Tobias Windisch <windisch@ovgu.de>
+# Date: January 2016. 
+##########################################################################################
+
+
+##########################################################################################
+#   PREREQUISITES: 
+##########################################################################################
+setwd("~/Dropbox/DynamicMarkov-p1/R code") #change this of course
+# Go and download the following file first: p1walk.R from this location: 
+# http://math.iit.edu/~spetrov1/DynamicP1supplement/p1walk.R 
+source('p1walk.R') # See comment on line 19 of GetCyclesWithZeros.R # 
+
+setwd("~/Dropbox/Tobias") #change this of course
+source('GetCyclesWithZeros.R')
+
+options(error=traceback)
+
+# I like to plot pictures in 2x2 format to see more than one at a time: 
+par(mfrow = c(2,2), mar=c(0,0,0,0)+0.1)#1, 4, 1, 1) + 0.1)#it goes c(bottom, left, top, right)
+##########################################################################################
+
+
+
+
+##########################################################################################
+# Example 1: Tobias' toy example 
+# This section sets up the graph and sets up the zeros
+##########################################################################################
+#
+# ------ The observed graph ------
+#
+# there are no directed edges:
+D.tob = graph.empty(8)
+# the graph is a 4x4 table from indep. model w/ structural zeros; so it's a subraph of a
+# undirected bipartite graph on 4+4 vertices - lets call them 1..4 and 5..8: 
+#{x1,y1},{x1,y1},{x1,y1},
+#{x2,y4},
+#{x3,y2}, {x3,y2}, {x3,y2}, {x3,y2}, {x3,y2}, {x3,y2}, {x3,y2},
+#{x4,y3}, {x4,y3}, {x4,y3}, {x4,y3}, {x4,y3}, {x4,y3}, {x4,y3}, {x4,y3}, {x4,y3}
+B.tob = graph(c(1,5, 1,5, 1,5, 2,8, 3,6, 3,6, 3,6, 3,6, 3,6, 3,6, 3,6, 4,7, 4,7, 4,7, 4,7, 4,7, 4,7, 4,7, 4,7, 4,7), d=FALSE)
+Plot.Mixed.Graph(D.tob, B.tob)
+# If i want to save the graph: 
+#write.table(Get.Configuration.Matrix(D.tob, B.tob), file="~/Dropbox/Tobias/the-graph-config-mtx.txt", col.names=F, row.names=F)
+#
+# ------ The parameter graph ------
+#
+#the underlying graph of the problem is
+#{x1,y1},{x1,y3},
+#{x2,y1},{x2,y3},{x2,y4},
+#{x3,y1},{x3,y2},{x3,y4},
+#{x4,y2},{x4,y3}
+# MISSING edges are structural zeros, but also note that the underlying graph must be bipartite. 
+#
+# Here is how to store this: 
+# Record the vector of edges of the underlying graph of the problem (i.e. list all 'legal' table cells; missing edges are structural zeros of the model.)
+# To not mess with the old code much, input the vertices of the graph in numerical order: so x1..x4 are 1..4 and y1..y4 are 5..8.
+parameter.graph.edges=c(1,5, 1,7, 2,5, 2,7, 2,8, 3,5, 3,6, 3,8, 4,6, 4,7)
+#num.vtces=8
+#
+# ------ Getting the zeros stored ------
+# 
+# To store them to use in the code below:
+zeros = Get.Structural.Zeros(parameter.graph.edges,num.vtces)
+#
+# and IF you would like to see them plotted: 
+zeros.graph=Get.Structural.Zeros.Graph(parameter.graph.edges,num.vtces)
+print("here is the set of forbidden edges")
+str(zeros.graph) 
+plot(zeros.graph, layout=layout.circle)
+##########################################################################################
+
+
+##########################################################################################
+# Running the example: 
+# This section uses the graphs set up above to produce moves 
+##########################################################################################
+# 
+# how many times would you like to try to make a move? 1 step often fails to construct a
+# nonzero move, so just do more
+steps.for.walk=150
+# initialize: 
+next.network = list(D.tob,B.tob)
+# start my plots from scratch:
+par(mfrow = c(2,2), mar=c(0,0,0,0)+0.1)#1, 4, 1, 1) + 0.1)#it goes c(bottom, left, top, right)
+Plot.Mixed.Graph(next.network[[1]],next.network[[2]]) 
+for(i in 1: steps.for.walk){
+  next.network = Get.Next.Network.Zeros(next.network[[1]],next.network[[2]],zeros); 
+  if (!next.network[[3]]) { 
+    print("I found an applicable move! Plotting. Moving on.")
+    print("------------------------------------------------")
+    Plot.Mixed.Graph(next.network[[1]],next.network[[2]]) 
+  }
+}
+##########################################################################################
+
+
+##########################################################################################
+# Other notes:
+# 
+# If it is helpful, I can also use Enumerate.Fiber to save these into a file - that way, 
+# you can have all the graphs visited in the current walk stored somewhere. 
+# 
+##########################################################################################
+
---
+++ GetCyclesWithZeros.R
@@ -0,0 +1,119 @@
+################################################################################################
+# This code loads all the necessary functions to generate one random applicable Markov move on a 
+# contingency table with structural zeros. 
+# It does so by taking input consisting of 1) an underlying graph of the problem (i.e., the 
+# parameter hypergraph of the log-linear model), and 2) the current table or graph as a list of 
+# edges. It returns one move, or an empty move if it gets unlucky in that one step. 
+# 
+# Load this file first, and then see ExamplesToGetCyclesWithZeros.R for how to run the code.
+# 
+# Author:   Sonja Petrovic <Sonja.Petrovic@iit.edu>
+# For: Tobias Windisch <windisch@ovgu.de>
+# Date: January 2016. 
+################################################################################################
+
+
+###### prerequisites 
+library("igraph")
+
+# Heads up: You need to ensure to first load p1walk.R -- see the Example file for the 'source' command. 
+
+
+#######################################################################
+# Get.Structural.Zeros
+# Input:  parameter hypergraph
+# Output: list of edges that are not allowed, 
+#         that is, that corrspond to structural zero cells in the table
+Get.Structural.Zeros<-function(parameter.graph){
+  zeros.graph=graph.complementer(graph(parameter.graph.edges,d=FALSE))
+  #num.vtces = length(V(graph(parameter.graph.edges)))
+  #zeros.graph=graph.complementer(graph(parameter.graph.edges,n=num.vtces,d=FALSE))
+  return(c(t(get.edgelist(zeros.graph))))
+}
+#######################################################################
+
+
+#######################################################################
+# Get.Structural.Zeros.Graph
+# Input:  parameter.graph = parameter hypergraph, the underlying graph of the problem
+# Output: the graph whose edges are the edges that are not allowed in the model, 
+#         that is, that corrspond to structural zero cells in the table
+Get.Structural.Zeros.Graph<-function(parameter.graph){
+  zeros.graph=graph.complementer(graph(parameter.graph.edges,d=FALSE))
+  return(zeros.graph)
+}
+#######################################################################
+
+
+
+#######################################################################
+# Get.Bidirected.Move.Zeros
+# Input:  
+#         d = directed graph (EMPTY in this set of examples, but remains as a general placeholder),
+#         b = undirected graph, representing the current graph / table, 
+#         zeros = a list of fobidden edges (in vector format)
+# Output: 
+#         A list of two vectors: edges to remove and edges to add to the current graph.
+# Notes: 
+#         The move this function outputs may be empty. 
+Get.Bidirected.Move.Zeros <- function(d, b, zeros) {
+  bidir.piece = Get.Bidirected.Piece(b)
+  if (is.null(bidir.piece[[1]])) 
+    return(list(graph.empty(vcount(b), directed=FALSE),graph.empty(vcount(b), directed=FALSE)))
+  else {
+    g.add = graph(bidir.piece[[2]],vcount(b), directed = FALSE)
+    g.remove = graph(bidir.piece[[1]], vcount(b),directed = FALSE)
+    # I ONLY NEED TO CHECK IF I'M ATTEMPTING TO ADD TO STRUCTURAL ZEROS,  WHICH IS NOT ALLOWED:
+    # edges.to.add does not intersect zeros [i.e. no edges placed into structural zerso!]:
+    if (!ecount(graph.intersection(graph(zeros,directed=FALSE), g.add)) == 0) 
+      return(list(graph.empty(vcount(b), directed=FALSE),graph.empty(vcount(b), directed=FALSE)))
+    }
+    return(list(g.remove, g.add))
+  }
+#######################################################################
+
+
+#######################################################################
+# Get.Next.Network.Zeros  
+# Input:  
+#         d = directed graph (EMPTY in this set of examples, but remains as a general placeholder),
+#         b = undirected graph, representing the current graph / table, 
+#         zeros = a list of fobidden edges (in vector format)
+# Output: 
+#         The network obtained by applying a random move obtained by Get.Bidirected.Move.Zeros
+#         true/false flag to indicated if a move was empty (no cycle produced on this attempt)
+# Notes:
+#         We may stay at the same place. 
+Get.Next.Network.Zeros <- function(d,b,zeros){        #},coin=c(0,1,0){
+  empty.move=TRUE
+  bidir.move = Get.Bidirected.Move.Zeros(d,b,zeros)
+  markov.move = list(graph.empty(vcount(d)),graph.empty(vcount(d)),bidir.move[[1]],bidir.move[[2]])
+  if (!ecount(markov.move[[3]])==0){ 
+    print("edges to remove:")
+    str(markov.move[[3]])
+    print("edges to add:")
+    str(markov.move[[4]])
+  }
+  if (!ecount(markov.move[[1]])==0 || !ecount(markov.move[[3]])==0){
+    empty.move=FALSE
+    new.directed.graph = d
+    #    new.bidirected.graph = graph.union(graph.difference(b,markov.move[[3]]),markov.move[[4]])
+    # oops have to worry about graph.union taking only simple graphs - it messes me up slightly. here is a quick fix:
+    new.bidirected.graph = graph.adjacency(get.adjacency(graph.difference(b,markov.move[[3]]))+get.adjacency(markov.move[[4]]),mode="undirected")
+    # I CAN DO WEIGHTED GRAPHS! see this: 
+    # http://stackoverflow.com/questions/31417071/graph-union-summing-edge-weights-attributes-igraph-r
+  } 
+  else{
+    #empty move, graphs unchanged
+    new.directed.graph = d
+    new.bidirected.graph = b
+  }
+  return(list(new.directed.graph,new.bidirected.graph,empty.move))		 
+}
+#######################################################################
+
+
+
+
+
+
