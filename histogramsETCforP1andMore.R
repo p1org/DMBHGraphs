ee9eb12235e3af4058761597f4ae5a343a927b28
Sondzus <sonjapetrovicbg@gmail.com>
2016-06-07 14:45:55 -0500

    turn off grid printing at close; also print GoFs if burn>0

--- histogramsETCforP1andMore.R
+++ histogramsETCforP1andMore.R
@@ -6,9 +6,9 @@
 #
 # Input: 
 #   - gof.values, a LIST OF LISTS? VECTOR OF LISTS? WHAT - list of values of the GoF statistic from a random walk, whose 1st entry is the OBSERVED ONE
-#   - p.values, a LIST OF LISTS? VECTOR OF LISTS? WHAT - list of p-value esitmates from a random walk
+#   - p.value.estimates, a LIST OF LISTS? VECTOR OF LISTS? WHAT - list of p-value esitmates from a random walk
 # 
-GoF.Testing.Plots <- function(){
+GoF.Simulations.Summary <- function(){
   # this is so that when we run 100 simulations, we get a mean p-value, 
   # and upper and lower quantiles for them, over the 100 simulations. 
   # clearly better than plotting 100 separate plots.
@@ -23,7 +23,7 @@ GoF.Testing.Plots <- function(){
 # 
 # Input: 
 #   - gof.values, a list of values of the GoF statistic from a random walk, whose 1st entry is the OBSERVED ONE
-#   - p.values, a list of p-value esitmates from a random walk
+#   - p.value.estimates, a list of p-value esitmates from a random walk
 #	Optional input:                                                       
 #   - filesave, a Boolean indicating whether to save plots to pdf file
 #   - filename, 
@@ -35,10 +35,10 @@ GoF.Testing.Plots <- function(){
 # Typical usage of this function:
 #   fiber.walk=Estimate.p.Value.for.Testing(D,B,steps.for.walk=10)
 #   gof.values = fiber.walk[[3]]
-#   p.values = fiber.walk[[2]]  
-#   GoF.Testing.Plots(gof.values,p.values,dataname="test data set",filesave=FALSE)
+#   p.value.estimates = fiber.walk[[2]]  
+#   GoF.Testing.Plots(gof.values,p.value.estimates,filesave=FALSE)
 ########################################################################
-GoF.Testing.Plots <- function(gof.values,p.values, dataname="Set title w/ option 'dataname'", burn=0, filesave=FALSE,filename='GoF testing plots', grid=c(2,2)){
+GoF.Testing.Plots <- function(gof.values,p.value.estimates, dataname="Set title w/ option 'dataname'", burn=0, filesave=FALSE,filename='GoF testing plots', grid=c(2,2)){
   
   print("This is still a little shitty b/c I don't check if input is of correct format..e.g., the two lists should be of same length if they come from the same simulation. For typical usage of the function, see function header @code. ")    
   
@@ -52,7 +52,7 @@ GoF.Testing.Plots <- function(gof.values,p.values, dataname="Set title w/ option
   par(mfrow = grid, mar=c(2,1,0,0)+3) # spacing; it goes c(bottom, left, top, right)  
   
   # 1) plot p-value estimates: 
-  plot(p.values, main=dataname,sub=paste(length(p.values),"steps"),  xlab="Length of Walk", ylab="p-values", ylim=c(0,1))
+  plot(p.value.estimates, main=dataname,sub=paste(length(p.value.estimates),"steps"),  xlab="Length of Walk", ylab="p-values", ylim=c(0,1))
   
   # 2) plot the sampling distribution of the GoF statistic:
   hist(gof.values,main=dataname, xlab="Goodness of Fit Statistic", ylab="frequency")
@@ -62,10 +62,14 @@ GoF.Testing.Plots <- function(gof.values,p.values, dataname="Set title w/ option
   if(burn>0){
     p.estimates = Estimate.p.Value.From.GoFs(gof.values, burn) 
     plot(p.estimates[[2]],  main=dataname ,sub=paste(length(p.estimates[[2]]),"steps"),  xlab=paste("Length of Walk (after", burn,"burn-in steps)"), ylab="p-values", ylim=c(0,1))    
+    hist(gof.values[burn+2:length(gof.values)],main=dataname, xlab="Goodness of Fit Statistic", ylab="frequency")
+    abline(v=gof.values[1], col="red")
   }
   
   if(filesave){
     print(paste("Plots are saved to",filename,".pdf in the working directory (check with getwd())."))
     dev.off()  # close file connection.
   }
+  # turn off grid printing:
+  par(mfrow = c(1,1))
 }
