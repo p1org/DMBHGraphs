5dec0899e393176dfa1bc817f425bbfc336697e2
sdgpfo <despina@dhcp92.merunorth104.iit.edu>
2016-06-28 14:22:48 -0500

    updated plot labels for visual reasons, also added an optional input plotlabel in case user wants specific plot labels. Added some error-checking for gdir.

--- Test.Model.Fit.R
+++ Test.Model.Fit.R
@@ -5,7 +5,7 @@
 # - Plots p-value convergence plots and gof histograms for each run both in a file and on for immediate viewing.
 # - Calculates the quartiles from the progressive p-value estimates and plots these as well.  
 ########
-Test.Model.Fit<-function(gdir, gbidir=graph.empty(vcount(gdir),directed=FALSE), foldername, numSteps, iterations, mleMatr=NULL, model, ignore.trivial.moves=FALSE, tol=0.001, maxiter=100000, testname, SBM.blocks=NULL){
+Test.Model.Fit<-function(gdir, gbidir=graph.empty(vcount(gdir),directed=FALSE), foldername, numSteps, iterations, mleMatr=NULL, model, ignore.trivial.moves=FALSE, tol=0.001, maxiter=100000, testname, plotlabel=NULL, SBM.blocks=NULL){
   if (model == "beta.SBM"){
     if (is.null(SBM.blocks) || !is.vector(SBM.blocks))       
       stop("beta.SBM model requires a non-empty vector SBM.blocks input." )     
@@ -17,10 +17,13 @@ Test.Model.Fit<-function(gdir, gbidir=graph.empty(vcount(gdir),directed=FALSE), 
         gdir = graph.empty(vcount(gdir), directed = TRUE)
       }
     }    
-  }else if (ecount(gbidir)==0){
-    mixed.graph = split.Directed.Graph(gdir)
-    gdir = mixed.graph[[1]]
-    gbidir = mixed.graph[[2]]  
+  }else{
+    if (!is.directed(gdir))stop("Test.Model.Fit: First input must be a directed igraph object.")
+    if (ecount(gbidir)==0){
+      mixed.graph = split.Directed.Graph(gdir)
+      gdir = mixed.graph[[1]]
+      gbidir = mixed.graph[[2]]  
+    }    
   }
   
   if (is.null(mleMatr)) { mleMatr = Get.MLE(gdir, gbidir, model, tol, maxiter, SBM.blocks=SBM.blocks) }
@@ -31,7 +34,7 @@ Test.Model.Fit<-function(gdir, gbidir=graph.empty(vcount(gdir),directed=FALSE), 
     trivs.iteration = "non-trivial-move iteration"
   }else {
     trivs.label = ""
-    trivs.filename = "."
+    trivs.filename = ""
     iteration = "iteration"
   }
   
@@ -41,6 +44,10 @@ Test.Model.Fit<-function(gdir, gbidir=graph.empty(vcount(gdir),directed=FALSE), 
   p.progressive.estimates = array(0,dim=c(iterations,numSteps))
   gof.values = array(0,dim=c(iterations,numSteps+1))
   minNumSteps = numSteps
+  
+  if (is.null(plotlabel)){
+    plotlabel = paste(testname, trivs.label, model)
+  }
   for (i in 1:iterations){
     cat(sprintf("iteration = %d\n",i))
     tmp = Estimate.p.Value.for.Testing(gdir, gbidir, steps=numSteps, model, ignore.trivial.moves, mleMatr, SBM.blocks=SBM.blocks)
@@ -49,12 +56,14 @@ Test.Model.Fit<-function(gdir, gbidir=graph.empty(vcount(gdir),directed=FALSE), 
     num.moves = length(tmp[[2]])
     p.progressive.estimates[i,1:num.moves] = tmp[[2]]
     gof.values[i,1:(num.moves+1)] =  tmp[[3]]
-    GoF.Testing.Plots(gof.values[i,1:(num.moves+1)], p.progressive.estimates[i,1:num.moves], filesave=TRUE, filename = sprintf("%s.iteration%d.figs.pdf",base.filename, i), grid=c(2,1), dataname = paste(testname,"; ", trivs.label," ", model))
-    GoF.Testing.Plots(gof.values[i,1:(num.moves+1)], p.progressive.estimates[i,1:num.moves], grid=c(2,1), dataname=paste(testname,"; ", trivs.label, " ", model))
+    GoF.Testing.Plots(gof.values[i,1:(num.moves+1)], p.progressive.estimates[i,1:num.moves], filesave=TRUE, filename = sprintf("%s.iteration%d.figs.pdf",base.filename, i), grid=c(2,1), dataname = plotlabel)
+    GoF.Testing.Plots(gof.values[i,1:(num.moves+1)], p.progressive.estimates[i,1:num.moves], grid=c(2,1), dataname=paste(testname, trivs.label, model))
     
     save(p.values, file=sprintf("%s.p.values.RData",base.filename))
     save(p.progressive.estimates, file=sprintf("%s.p.progressive.estimates.values.RData",base.filename))
     save(gof.values, file=sprintf("%s.gof.values.values.RData",base.filename))
+    
+    
     
     cat(sprintf("mean(p.values[1:%d]) = %f | median(p.values[1:%d]) = %f | var(p.values[1:%d]) = %f\n", i, mean(p.values[1:i]), i, median(p.values[1:i]), i, var(p.values[1:i])))
     if (ignore.trivial.moves) 
