b4abbe540b2377d7acb1fa057c1c649ae79dcabd
csprock <csprock@gmail.com>
2019-12-14 13:08:54 -0600

    added structural_zeros_matrix() helper function and unit test, moved expected_array_equal() unit test helper function to new helper_functions.R

--- tests/testthat/test_configuration_matrices.R
+++ tests/testthat/test_configuration_matrices.R
@@ -2,36 +2,6 @@ testthat::context("Test Get.Configuration.Matrix function family")
 library(igraph)
 
 
-expect_array_equal <- function(object, expected){
-
-  format_indices <- function(x){
-    # helper function that formats matrix indices for printing,
-    # indices contained along rows of matrix
-    formatted_indices <- list()
-    for (i in 1:dim(x)[1]){
-      formatted_indices[[i]] <- paste0("(", paste(x[i,], collapse=","), ")")
-    }
-    return(formatted_indices)
-  }
-
-  act <- testthat::quasi_label(rlang::enquo(object))
-
-  non_zero_indices <- which(object - expected != 0, arr.ind=TRUE)
-
-  status <- TRUE
-  test_message <- ""
-
-  if (length(non_zero_indices) > 0){
-    status <- FALSE
-    test_message <- paste("Nonzero index", format_indices(non_zero_indices), collapse=": ")
-  }
-
-  testthat::expect(ok=status, failure_message=test_message)
-
-  invisible(act$val)
-}
-
-
 testthat::test_that(
   "Test that Get.Configuration.Matrix.p1.FW returns correct matrix on toy example", {
 
