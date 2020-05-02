5ecf9004273c2cb7f2c0f35d4f63f0ffad341f5f
csprock <csprock@gmail.com>
2020-05-02 13:11:47 -0500

    added reverse_walk function and unit test

---
+++ R/bipartite_walk.R
@@ -0,0 +1,16 @@
+
+reverse_walk <- function(edges){
+
+  m <- nrow(edges)
+  new_edges <- matrix(nrow=m, ncol=2)
+  
+  new_edges[m, 1] <- edges[1, 1]
+  new_edges[m, 2] <- edges[m, 2]
+  
+  for (i in 1:(m-1)){
+    new_edges[i, 1] <- edges[i+1,1]
+    new_edges[i, 2] <- edges[i, 2]
+  }
+  
+  return(new_edges)
+}
