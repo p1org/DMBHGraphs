7e5a95ea703db7fd04b8090d02bc4679cd1540a3
csprock <csprock@gmail.com>
2020-06-21 12:11:02 -0500

    cleanup code to satisfy linter

--- tests/testthat/test_bipartite_walk.R
+++ tests/testthat/test_bipartite_walk.R
@@ -5,23 +5,21 @@ library(igraph)
 testthat::test_that(
   "Test that reversed_walk() function is working correctly",
   {
-    
     edges <- matrix(c(
-      c(1,3),
-      c(2,4),
-      c(3,5),
-      c(4,6)
-    ), nrow=4, byrow=TRUE)
+      c(1, 3),
+      c(2, 4),
+      c(3, 5),
+      c(4, 6)
+    ), nrow = 4, byrow = TRUE)
     
     expected_result <- matrix(c(
-      c(2,3),
-      c(3,4),
-      c(4,5),
-      c(1,6)
-    ), nrow=4, byrow=TRUE)
+      c(2, 3),
+      c(3, 4),
+      c(4, 5),
+      c(1, 6)
+    ), nrow = 4, byrow = TRUE)
     
     result <- reverse_walk(edges)
-    
     expect_array_equal(expected_result, result)
   }
 )
@@ -30,21 +28,20 @@ testthat::test_that(
   "Test that bipartite_walk returns the reversed walk when there are no zeros and the graph is simple.",
   {
     edges <- matrix(c(
-      c(1,3),
-      c(2,4),
-      c(3,5),
-      c(4,6)
-    ), nrow=4, byrow=TRUE)
+      c(1, 3),
+      c(2, 4),
+      c(3, 5),
+      c(4, 6)
+    ), nrow = 4, byrow = TRUE)
     
     expected_result <- matrix(c(
-      c(2,3),
-      c(3,4),
-      c(4,5),
-      c(1,6)
-    ), nrow=4, byrow=TRUE)
+      c(2, 3),
+      c(3, 4),
+      c(4, 5), 
+      c(1, 6)
+    ), nrow = 4, byrow = TRUE)
     
     result <- bipartite_walk(edges)
-    
     expect_array_equal(expected_result, result)
   }
 )
@@ -53,22 +50,21 @@ testthat::test_that(
   "Test that bipartite_walk returns NULL when it tries to add a forbidden edge",
   {
     edges <- matrix(c(
-      c(1,3),
-      c(2,4),
-      c(3,5),
-      c(4,6)
-    ), nrow=4, byrow=TRUE)
+      c(1, 3),
+      c(2, 4),
+      c(3, 5),
+      c(4, 6)
+    ), nrow = 4, byrow = TRUE)
 
     zeros.graph <- igraph::graph_from_edgelist(
      matrix(c(
-      c(2,3),
-      c(3,4),
-      c(4,5),
-      c(1,6)), nrow=4, byrow=TRUE)
+      c(2, 3),
+      c(3, 4),
+      c(4, 5),
+      c(1, 6)), nrow = 4, byrow = TRUE)
     )
 
-    result <- bipartite_walk(edges, zeros.graph=zeros.graph)
-
+    result <- bipartite_walk(edges, zeros.graph = zeros.graph)
     testthat::expect_null(result)
   }
 )
@@ -77,15 +73,14 @@ testthat::test_that(
   "Test that bipartite_walk returns NULL when resulting subgraph is not simple",
   {
     edges <- matrix(c(
-      c(1,3),
-      c(2,4),
-      c(4,2),
-      c(3,5),
-      c(4,6)
-    ), nrow=5, byrow=TRUE)
+      c(1, 3),
+      c(2, 4),
+      c(4, 2),
+      c(3, 5),
+      c(4, 6)
+    ), nrow = 5, byrow = TRUE)
 
     result <- bipartite_walk(edges)
-
     testthat::expect_null(result)
   }
 )
