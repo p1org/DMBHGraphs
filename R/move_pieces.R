6b23722f666968198b3afd8a364b9f0ecd6cb69a
csprock <csprock@gmail.com>
2020-06-27 20:40:37 -0500

    added move_pieces.R file and helper functions

---
+++ R/move_pieces.R
@@ -0,0 +1,52 @@
+
+sample_edges <- function(g, small.moves.coin = NULL) {
+
+    edges <- igraph::E(g)
+
+    subset_size <- sample(2:igraph::ecount(g), 1)
+
+    if (!is.null(small.moves.coin)) {
+        if (runif(1) < small.moves.coin) {
+            subset_size <- sample(2:4, 1) # is 4 a hard-coded parameter? 
+        }
+    }
+
+    edge_sample <- sample(edges, subset_size)
+    return(edge_sample)
+}
+
+
+recursive_partition <- function(edges) {
+
+    n <- length(edges)
+
+    if (n == 2 || n == 3) {
+        return(list(edges))
+    } else if (n == 4) {
+
+        e1 <- edges[1:2]
+        e2 <- edges[3:4]
+
+        return(list(e1, e2))
+    } else {
+        
+        j <- sample(3:(n - 1))
+        e1 <- edges[1:(j - 1)]
+        e2 <- edges[j:n]
+
+        return(list(list(e1), recursive_partition(e2)))
+    }
+}
+
+
+
+# Source: https://stackoverflow.com/questions/8139677/how-to-flatten-a-list-to-a-list-without-coercion/8139959#8139959
+flatten_list <- function(x) {
+  len <- sum(rapply(x, function(x) 1L))
+  y <- vector('list', len)
+  i <- 0L
+  rapply(x, function(x) { i <<- i+1L; y[[i]] <<- x })
+  y
+}
+
+
