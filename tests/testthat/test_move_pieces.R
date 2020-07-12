44150b938f3a905312674d21a888ede3fe4edbb6
csprock <csprock@gmail.com>
2020-07-11 21:37:06 -0500

    added test to test that sample_edges returned an igraph.es object

---
+++ tests/testthat/test_move_pieces.R
@@ -0,0 +1,15 @@
+testthat::context("Test functions used in the move_pieces.R file")
+library(igraph)
+
+
+
+
+testthat::test_that(
+    "Test that sample_edges returns an igraph.es object", 
+    {
+        G <- igraph::erdos.renyi.game(n = 10, p.or.m = 15, directed = TRUE, type = "gnm")
+        edge_sample <- sample_edges(G)
+
+        testthat::expect_s3_class(edge_sample, "igraph.es")
+    }
+)
