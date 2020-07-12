4b923f845efc86265da24825505983a04c1d0008
csprock <csprock@gmail.com>
2020-07-11 22:13:11 -0500

    added test for output sizes of recursive_partition for the cases where n=2,3,4

--- tests/testthat/test_move_pieces.R
+++ tests/testthat/test_move_pieces.R
@@ -13,3 +13,25 @@ testthat::test_that(
         testthat::expect_s3_class(edge_sample, "igraph.es")
     }
 )
+
+testthat::test_that(
+    "Test that recursive_partition returns correct output lengths for n=2,3,4",
+    {
+        G2 <- igraph::erdos.renyi.game(n = 3, p.or.m = 2, directed = TRUE, type = "gnm")
+        G3 <- igraph::erdos.renyi.game(n = 3, p.or.m = 3, directed = TRUE, type = "gnm")
+        G4 <- igraph::erdos.renyi.game(n = 4, p.or.m = 4, directed = TRUE, type = "gnm")
+
+        edges2 <- recursive_partition(E(G2))
+        edges3 <- recursive_partition(E(G3))
+        edges4 <- recursive_partition(E(G4))
+
+        testthat::expect_length(edges2, 1)
+        testthat::expect_length(edges3, 1)
+        testthat::expect_length(edges4, 2)
+        testthat::expect_length(edges2[[1]], 2)
+        testthat::expect_length(edges3[[1]], 3)
+        testthat::expect_length(edges4[[1]][[1]], 2)
+        testthat::expect_length(edges4[[2]][[1]], 2)
+    }
+)
+
