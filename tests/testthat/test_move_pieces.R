db4c70995c02ee369de03a3271563f643c3accdb
csprock <csprock@gmail.com>
2020-07-11 22:59:40 -0500

    added unit test for when check_intersections is supposed to return FALSE

--- tests/testthat/test_move_pieces.R
+++ tests/testthat/test_move_pieces.R
@@ -88,3 +88,24 @@ testthat::test_that(
         testthat::expect_true(!check_mutual_edges(G, r, b))
     }
 )
+
+
+testthat::test_that(
+    "Test that check_intersection returns FALSE when expected", 
+    {
+        G1 <- igraph::graph_from_edgelist(
+            matrix(c(
+            c(1, 4),
+            c(4, 2),
+            c(2, 1),
+            c(1, 3)
+        ), ncol = 2, byrow = TRUE), directed = FALSE)
+
+        G2 <- graph_from_edgelist(matrix(c(
+            c(2, 1),
+            c(4, 2)
+        ), ncol = 2, byrow = TRUE))
+
+        testthat::expect_true(!check_intersection(G1, G2))
+    }
+)
