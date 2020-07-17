e557cd990a3abb4509aad73e76ba3473468c53b3
csprock <csprock@gmail.com>
2020-07-16 21:39:43 -0500

    changed unit tests to handle cases where zeros graphs is directed and undirected

--- tests/testthat/test_bipartite_walk.R
+++ tests/testthat/test_bipartite_walk.R
@@ -47,7 +47,31 @@ testthat::test_that(
 )
 
 testthat::test_that(
-  "Test that bipartite_walk returns NULL when it tries to add a forbidden edge",
+  "Test that bipartite_walk returns NULL when it tries to add a forbidden edge when zeros graph is directed",
+  {
+    edges <- matrix(c(
+      c(1, 3),
+      c(2, 4),
+      c(3, 5),
+      c(4, 6)
+    ), nrow = 4, byrow = TRUE)
+
+    zeros.graph <- igraph::graph_from_edgelist(
+     matrix(c(
+      c(2, 3),
+      c(3, 4),
+      c(4, 5),
+      c(1, 6)), nrow = 4, byrow = TRUE), directed = FALSE
+    )
+
+    result <- bipartite_walk(edges, zeros.graph = zeros.graph)
+    testthat::expect_null(result)
+  }
+)
+
+
+testthat::test_that(
+  "Test that bipartite_walk returns NULL when it tries to add a forbidden edge when zeros graph is undirected",
   {
     edges <- matrix(c(
       c(1, 3),
@@ -68,6 +92,7 @@ testthat::test_that(
     testthat::expect_null(result)
   }
 )
+
 
 testthat::test_that(
   "Test that bipartite_walk returns NULL when resulting subgraph is not simple",
