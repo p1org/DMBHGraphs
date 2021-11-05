393005abb19a760f2fb9af4af5698cb8fb54c278
csprock <csprock@gmail.com>
2021-11-05 10:56:15 -0700

    added documentation to reciprocated_edges() and split_directed() functions

--- R/utils.R
+++ R/utils.R
@@ -56,6 +56,16 @@ validate_bidirected_graph_part <- function(g, type){
   validate_simple(g, type)                # check if graph is simple
 }
 
+
+
+#' Finds reciprocated edges
+#' 
+#' Finds reciprocated edges by checking if the sending vertex u
+#' is in the out-neighborhood of its out-neighbors, i.e. is u in N(v) for v in N(u)
+#' 
+#' @param g igraph directed graph
+#' 
+#' @return vector of booleans
 reciprocated_edges <- function(g) {
   apply(
     X=igraph::ends(g, igraph::E(g), names=FALSE),
@@ -65,11 +75,20 @@ reciprocated_edges <- function(g) {
   )
 }
 
+#' Splits graph into reciprocated and unreciprocated parts
+#' 
+#' The graph containing the reciprocated edges is represented by an igraph undirected graph object. 
+#' The graph containing the unreciprocated edges is represented by an igraph directed graph. 
+#' 
+#' @param g igraph directed graph
+#' 
+#' @return list(igraph undirected graph, igraph directed graph)
 split_directed <- function(g) {
   recip_idx <- reciprocated_edges(g)
   return(
     list(
-      gudir=igraph::as.undirected(igraph::subgraph.edges(g, igraph::E(g)[recip_idx], delete.vertices=FALSE), mode="collapse"),
+      gudir=igraph::as.undirected(
+        igraph::subgraph.edges(g, igraph::E(g)[recip_idx], delete.vertices=FALSE), mode="collapse"),
       gdir=igraph::subgraph.edges(g, igraph::E(g)[!recip_idx], delete.vertices=FALSE)
     )
   )
